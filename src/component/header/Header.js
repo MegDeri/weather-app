43dce04cd98064e1f7067b9d35eff2555f73706e
Magda Aderibigbe <magdaaderibigbe@gmail.com>
2020-03-14 16:10:11 +0200

    Add icons to Header

--- src/component/header/Header.js
+++ src/component/header/Header.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBatteryFull, faWifi, faCircle } from "@fortawesome/free-solid-svg-icons";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Container, Row, Col } from "reactstrap";
@@ -9,13 +11,28 @@ export const Header = props => {
   return (
     <div className="navMain">
       <Container>
-        <Row>
+        <Row className="headerRow">
           <Col xs={12} md={6}>
-            <div>left</div>
+            <div>
+              Sketch &nbsp;
+              <span>
+                <FontAwesomeIcon className="wifi" icon={faWifi} />&nbsp;&nbsp;
+              </span>
+              <FontAwesomeIcon className="dot" icon={faCircle} />
+              <FontAwesomeIcon className="dot" icon={faCircle} />
+              <FontAwesomeIcon className="dot" icon={faCircle} />
+              <FontAwesomeIcon className="dot" icon={faCircle} />
+              <FontAwesomeIcon className="dot" icon={faCircle} />
+            </div>
           </Col>
-          <Col xs={12} md={6}>
-            <div>right</div>
+          <Col className="right" xs={12} md={6}>
+            <div><FontAwesomeIcon icon={faBatteryFull} /> 10:41</div>
           </Col>
+        </Row>
+        <Row className="second">
+          <div className="navHeader">
+            Säätutka
+          </div>
         </Row>
       </Container>
     </div>
