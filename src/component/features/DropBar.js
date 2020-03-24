3b6d21045c0ff95ab02f324635820a81cad86a7f
Magda Aderibigbe <magdaaderibigbe@gmail.com>
2020-03-24 16:13:35 +0200

    Add styles to dropdown component

--- src/component/features/DropBar.js
+++ src/component/features/DropBar.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
+import './DropBar.scss';
 
 const DropBar = (props) => {
   const [dropdownOpen, setDropdownOpen] = useState(false);
@@ -7,24 +8,27 @@ const DropBar = (props) => {
   const toggle = () => setDropdownOpen(prevState => !prevState);
 
   return (
-    <Dropdown isOpen={dropdownOpen} toggle={toggle}>
-      <DropdownToggle caret>
-        Kaikki kaupungit!
-      </DropdownToggle>
-      <DropdownMenu right>
-        <DropdownItem
-          id="658225,655195,650225,634964&units=metric"
-          value="group"
-          onClick={props.getWeather}
-        >
+    <div className="feature-container">
+      <Dropdown isOpen={dropdownOpen} toggle={toggle}>
+        <DropdownToggle caret>
           Kaikki kaupungit!
-        </DropdownItem>
-        <DropdownItem id="634964" value="weather" onClick={props.getWeather}>Tampere</DropdownItem>
-        <DropdownItem id="658225" value="weather" onClick={props.getWeather}>Helsinki</DropdownItem>
-        <DropdownItem id="655195" value="weather" onClick={props.getWeather}>Jyvaskila</DropdownItem>
-        <DropdownItem id="650225" value="weather" onClick={props.getWeather}>Kuopio</DropdownItem>
-      </DropdownMenu>
-    </Dropdown>
+        </DropdownToggle>
+        <DropdownMenu right>
+          <DropdownItem
+            id="658225,655195,650225,634964&units=metric"
+            value="group"
+            onClick={props.getWeather}
+          >
+            Kaikki kaupungit!
+          </DropdownItem>
+          <DropdownItem id="634964" value="weather" onClick={props.getWeather}>Tampere</DropdownItem>
+          <DropdownItem id="658225" value="weather" onClick={props.getWeather}>Helsinki</DropdownItem>
+          <DropdownItem id="655195" value="weather" onClick={props.getWeather}>Jyvaskila</DropdownItem>
+          <DropdownItem id="650225" value="weather" onClick={props.getWeather}>Kuopio</DropdownItem>
+        </DropdownMenu>
+      </Dropdown>
+    </div>
+
   );
 }
 
